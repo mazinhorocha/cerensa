8aa724bfed9848234eaef81561ab09a9227b2cb6
jandimar rocha <mazinhozr@gmail.com>
2021-08-09 00:38:04 -0300

    first commit

---
+++ pages/cliente/detail.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="2" md="2" sm="12">
+        <v-text-field v-model="id" label="ID" dense></v-text-field>
+      </v-col>
+      <v-col cols="6" md="6" sm="12">
+        <v-text-field v-model="name" label="Nome" dense></v-text-field>
+      </v-col>
+      <v-col cols="4" md="4" sm="12">
+        <v-text-field v-model="type" label="Tipo" dense></v-text-field>
+      </v-col>
+      <v-col cols="4" md="4" sm="12">
+        <v-text-field v-model="federalDocument" label="CPF/CNPJ" dense></v-text-field>
+      </v-col>
+      <v-col cols="4" md="4" sm="12">
+        <v-text-field v-model="stateDocument" label="RG/IE" dense></v-text-field>
+      </v-col>
+      <v-col cols="4" md="4" sm="12">
+        <v-text-field v-model="birthDate" label="Nascimento" dense></v-text-field>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12">
+        <h3>Endereço</h3>
+      </v-col>
+      <v-col cols="3" md="3" sm="12">
+        <v-text-field v-model="zipCode" label="CEP" dense></v-text-field>
+      </v-col>
+      <v-col cols="6" md="6" sm="12">
+        <v-text-field v-model="street" label="Rua" dense></v-text-field>
+      </v-col>
+      <v-col cols="3" md="3" sm="12">
+        <v-text-field v-model="number" label="Número" dense></v-text-field>
+      </v-col>
+      <v-col cols="6" md="6" sm="12">
+        <v-text-field v-model="complement" label="Complemento" dense></v-text-field>
+      </v-col>
+      <v-col cols="6" md="6" sm="12">
+        <v-text-field v-model="district" label="Bairro" dense></v-text-field>
+      </v-col>
+      <v-col cols="6" md="6" sm="12">
+        <v-text-field v-model="city" label="Cidade" dense></v-text-field>
+      </v-col>
+      <v-col cols="6" md="6" sm="12">
+        <v-text-field v-model="state" label="Estado" dense></v-text-field>
+      </v-col>
+      <v-col cols="12" md="12" sm="6">
+        <v-btn class="success">Salvar</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+
+
+<script>
+export default {
+  data(){
+    return {
+      id: null,
+      name: null,
+      type: null,
+      federalDocument: null,
+      stateDocument: null,
+      birthDate: null,
+      zipCode: null,
+      street: null,
+      number: null,
+      complement: null,
+      district: null,
+      city: null,
+      state: null,
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col cols="6" sm="12" md="6">
+        <h1>A Plataforma de gestão e análise de sustentabilidade para empresas de qualquer tamanho</h1>
+      </v-col>
+      <v-col cols="6" md="6" sm="12">
+      <v-card>
+          <v-img src="https://cerensa.com/wp-content/uploads/2020/06/SS_RelatorioIntegrado_Export_D-1536x864.png"></v-img>
+      </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6" offset="3">
+        <p class="text-center">
+          Cerensa® é a solução que unifica todos os módulos de sustentabilidade. Projetada para atender clientes de pequeno porte a grandes corporações que necessitam processar volumes elevados de informações e gerenciar operações distribuídas.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
